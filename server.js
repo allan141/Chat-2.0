3815171901e677e81d6fd94808d0726247c2ae09
allan141 <allancavalcante141@gmail.com>
2025-03-28 20:28:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ io.on("connection", (socket) => {
 });
 
 // Porta do servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
